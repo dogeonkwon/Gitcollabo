87643759555427906d6c854148e59396fba572be
권도건 <rnsjquddbs@gmail.com>
2022-02-26 02:15:49 +0900

    no pain no gain

--- dogun/0226_KDG/2309_일곱_난쟁이/s1.py
+++ dogun/0226_KDG/2309_일곱_난쟁이/s1.py
@@ -2,57 +2,43 @@
 # 2022-02-26
 
 
-def long(sub):
-    cnt = 0
-    for _ in sub:
-        cnt += 1
-    return cnt
-
-
-def func(i, N, s, t):    # i - 시작 원소인덱스 // N - 원소의 개수 // s - 이전까지 고려된 원소의 합 // t - 목표값
-    global lst
-    if s == t:                  # 목표값을 찾으면
-        sub = list()
-        for j in range(N):              # 비트가 1인지 아닌지 검사하기 위해 N번 만큼 반복문 가동
-            if bit[j]:                  # 비트가 1이라면 같은 자리의 a리스트의 j번째 번호를 출력
-                sub.append(arr[j])
-        lst.append(sub)
-
-    elif i == N:                  # 더이상 고려할 원소가 없으면
-        return
-    elif s > t:                  # 고려한 원소의 합 s가 이미 목표를 초과한 경우
-        return
-    else:                           # 비트가 처음에는 0이었으므로 1로 바꿔주고 검사하고
-        bit[i] = 1
-        func(i+1, N, s + arr[i], t)
-        bit[i] = 0                  # 다시 0으로 바꿔서 검사해본다.
-        func(i+1, N, s, t)
+# 합계를 구하는 함수
+def my_sum(s):
+    tal = 0
+    for v in s:
+        tal += v
+    return tal
 
 
 N = 9                                  # 원소의 개수만큼
 
 arr = list()
 for _ in range(9):
-    arr += map(int, input().split())
+    arr += map(int, input().split())        # arr에 +를 해주면서 입력값을 받아온다
 
-bit = [0]*N                             # 이진수 동작할 bit 리스트
-lst = list()                            # 조건을 만족하는 부분 집합의 리스트들을 모아둘 곳
-t = 100                                  # t(목표값)가 되는 경우가 있는가?
-func(0, N, 0, t)                           # 함수는 내가 어떤 조건을 달기 나름 / 조건을 잘 달수록 연산횟수가 줄어듬
+lst = list()            # 뺄 숫자를 담아줄 리스트
+result = list()         # 결과값을 담아줄 리스트
+
+for i in range(9):          # 9개의 값이 있으므로 1번씩 다 넣어준다.
+    total = my_sum(arr)
+    total -= arr[i]
+    j = 0
+    while j < 9:            # 그리고 같은 값은 제외하고 빼서 100이 되는 값을 찾아준다.
+        if j == i:
+            j += 1
+            continue
+        elif total - arr[j] == 100:
+            lst = [arr[i], arr[j]]
+            result = [k for k in arr if k not in lst]       # 찾은 값과 겹치지 않는 값들을 result에 모아준다.
+            break
+        else:
+            j += 1
 
 
-for i in lst:
-    if long(i) == 7:
-        i.sort()
-        for j in i:
-            print(j)
+for n in range(6):                  # result를 정렬
+    for m in range(n+1, 7):
+        if result[n] > result[m]:
+            result[n], result[m] = result[m], result[n]
 
-# 99
-# 100
-# 1
-# 2
-# 3
-# 4
-# 5
-# 6
-# 79
+for l in result:
+    print(l)
