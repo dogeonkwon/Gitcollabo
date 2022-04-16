5b0d7d7186c2b28c128e03e93c6670b9b51ed1f3
gitSongyun <baesongyun@gmail.com>
2022-04-16 10:13:33 +0900

    0416 study

--- songyun/0416/5014_스타트링크 (BFS)/s1.py
+++ songyun/0416/5014_스타트링크 (BFS)/s1.py
@@ -13,35 +13,32 @@ visited = [0] * (F+1)
 # 시작 층 방문
 visited[S] = 1
 
-# 최대 층 수
-result = F
-
+# 결과값
+result = 0
 while queue:
-    # s : 현재 위치, cnt : 몇 번 만에 방문 했는지
-    s, cnt = queue.popleft()
+    # current : 현재 위치, cnt : 몇 번 만에 방문 했는지
+    current, cnt = queue.popleft()
 
     # 목표지점에 도착 했을 때
-    if s == G:
-        result = cnt #
+    if current == G:
+        result = cnt
         break
 
-    # 위층으로 움직였을 때 범위 안에 들어오고, 아직 방문하지 않은 곳일 때
-    if s + U <= F and not visited[s+U]:
+    # 위층으로 움직였을 때, 범위 안에 들어오고, 아직 방문하지 않은 곳일 때
+    if current + U <= F and not visited[current+U]:
         # 그곳을 방문 처리하고
-        visited[s+U] = 1
+        visited[current+U] = 1
         # 다음 층과 cnt 에 +1을 한다.
-        queue.append((s+U, cnt+1))
+        queue.append((current+U, cnt+1))
 
-    if s - D >= 1 and not visited[s-D]:
-        visited[s-D] = 1
-        queue.append((s-D, cnt+1))
-    print(queue, cnt)
+    if current - D >= 1 and not visited[current-D]:
+        visited[current-D] = 1
+        queue.append((current-D, cnt+1))
 
+    print(queue)
 
-# while 문이 다 도는 동안 result 의 가 F 그대로인 경우
-if result == F:
+if result > 0:
+    print(result)
+else:
     print('use the stairs')
 
-
-else:
-    print(result)
